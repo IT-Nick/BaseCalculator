0c48954c1798d54934eaf2046362bc4eaf9aecd7
Stepan <stepan-pl@protonmail.com>
2022-02-24 00:19:20 +0300

    Little bit changes

--- saver.h
+++ saver.h
@@ -1,18 +1,18 @@
 class Saver{
 public:
     string GetSave(int value) {
-        return d[value];
-    }
-    void SetSave(const string& new_saveLastExpression) {
-        saveLastExpression = new_saveLastExpression;
-    }
-    string GetLastExpr() const {
-        return saveLastExpression;
-    }
-    void Insert(int choice) {
-        d.insert(d.begin() + choice, saveLastExpression);
-    }
+    return d[value];
+}
+void SetSave(const string& new_saveLastExpression) {
+    saveLastExpression = new_saveLastExpression;
+}
+string GetLastExpr() const {
+    return saveLastExpression;
+}
+void Insert(int choice) {
+    d.insert(d.begin() + choice, saveLastExpression);
+}
 private:
-    string saveLastExpression;
-    deque<string> d = {"nothing1", "nothing2", "nothing3", "nothing4"};
+string saveLastExpression;
+deque<string> d = {"nothing1", "nothing2", "nothing3", "nothing4"};
 };
